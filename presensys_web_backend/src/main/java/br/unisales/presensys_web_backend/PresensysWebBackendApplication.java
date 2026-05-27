241d4317360d67254504858a5bbcdb0633d3386a
Ester Soares Serafim <estersoaresserafim13a@gmail.com>
2026-05-27 09:48:46 -0300

    initial commit backend web

---
+++ presensys_web_backend/src/main/java/br/unisales/presensys_web_backend/PresensysWebBackendApplication.java
@@ -0,0 +1,13 @@
+package br.unisales.presensys_web_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresensysWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresensysWebBackendApplication.class, args);
+	}
+
+}
---
+++ presensys_web_backend/src/test/java/br/unisales/presensys_web_backend/PresensysWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unisales.presensys_web_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresensysWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
